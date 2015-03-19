dd645a7d34e415f8ff8392440df7ec55efba4fde
Maurice Dalderup <Maurice_Dalderup@hotmail.com>
2015-03-19 11:39:06 +0100

    Aanpassen van config files voor CI
    ^

--- CodeIgniter/application/config/config.php
+++ CodeIgniter/application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost/Web_Advanced/CodeIgniter/';
+$config['base_url'] = 'http://localhost/WebAdvanced_Project/CodeIgniter/';
 
 /*
 |--------------------------------------------------------------------------
